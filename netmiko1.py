9d86f4d91fd870018ad4204306670e23eb34277b
initesh <initesh@yahoo.com>
2021-11-29 14:24:35 -0800

    Updated Password Parameters in Netmiko1

--- netmiko1.py
+++ netmiko1.py
@@ -1,16 +1,18 @@
 from netmiko import ConnectHandler
 from getpass import getpass 
+import os 
+password = os.getenv("PYNET_PASSWORD") if os.getenv("PYNET_PASSWORD") else getpass()
 nxos1 = {
     "host" : 'nxos1.lasthop.io',
     "username" : 'pyclass',
-    "password" : getpass(),
+    "password" : password,
     "device_type" : 'cisco_nxos',
     # "session_log": 'my_session.txt',
 }
 nxos2 = {
     "host": "nxos2.lasthop.io",
     "username": "pyclass",
-    "password": getpass(),
+    "password": password,
     "device_type": "cisco_nxos",
 }
 for device in (nxos1, nxos2):
